e61e0414fcfcad3b5fc2066b7b87834f3c23ce1f
Luigi G. Marchetti <92608635+LuigiMarchetti@users.noreply.github.com>
2022-05-11 16:00:22 -0300

    Add files via upload

--- Uni05Ex05.java
+++ Uni05Ex05.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Uni05Ex05{
     public static void main(String[] args) {
         Scanner readLine = new Scanner (System.in);
         int n = readLine.nextInt();
         double sequencia = 8;
+        DecimalFormat df = new DecimalFormat("0");
         
         if(n >= 2){
 
         
         n -= 1;
-        System.out.println(sequencia);
+        System.out.print(df.format(sequencia)+ " ");
             for(int i = 0; i < n; i++){
                 if(i % 2 == 0){
                  
-                   System.out.println(sequencia+ 2);
+                   System.out.print(df.format(sequencia+ 2)+ " ");
                 }
                 else{
              
-                    System.out.println(sequencia * 2);
+                    System.out.print(df.format(sequencia * 2)+ " ");
                     sequencia = sequencia *2;
                 }
-       
-                
             }
       
         }
